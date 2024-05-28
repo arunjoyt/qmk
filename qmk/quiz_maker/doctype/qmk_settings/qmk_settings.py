1501ec8065a30ca4b2a04e4274cde08b31101ef4
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-05-28 14:40:29 +0530

    basic port from lhs app

---
+++ qmk/quiz_maker/doctype/qmk_settings/qmk_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Arun Joy Thekkiniyath and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class QMKSettings(Document):
+	pass
